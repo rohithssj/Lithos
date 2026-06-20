ffcad75d92bf530a96beee210594e283d6906297
rohithssj <rohithssj23@gmail.com>
2026-06-20 16:06:33 +0530

    Day-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { AxesHelper } from "three";
 import Mineral from './components/Mineral'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 const App = () => {
   return (
-    <div>
-      <Mineral />
+    <div className='w-screen h-screen bg-black'>
+      <Canvas camera={{ position: [0, 2, 8], fov: 50 }}>
+        <ambientLight intensity={2} />
+        <directionalLight position={[5, 5, 5]} intensity={3} />
+        <Mineral />
+        <OrbitControls />  
+        </Canvas>
     </div>
   )
 }
--- src/components/Mineral.jsx
+++ src/components/Mineral.jsx
@@ -1,38 +1,19 @@
-import React, { useRef } from 'react'
-import {Canvas, useFrame} from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { Center, useGLTF } from "@react-three/drei";
+import { useEffect } from "react";
+
 const Mineral = () => {
+  const model = useGLTF("/models/mineral_aragonite.glb");
 
-  const Animate = ({size,color})=>{
-    const ref = useRef()
 
-    useFrame((state,delta)=>{
-    ref.current.rotation.x+=delta *0.5
-    ref.current.rotation.y+=delta *0.6
-    ref.current.rotation.z+=delta *0.6
-
-    // ref.current.position.z += Math.sin(state.clock.elapsedTime)*0.2
-    })
-
-    return(
-      <mesh ref={ref}>
-        <boxGeometry args={size}/>
-      <meshStandardMaterial color={color} roughness={0.5} metalness={2}/>
-
-      </mesh>
-    )
-  }
   return (
-    <div className='w-screen h-screen bg-black'>
-      <Canvas camera={{position:[0,0,5], fov:50}}>
-        <ambientLight intensity={1.2}/>
-        <directionalLight position={[5,3,1]} intensity={2}/>
-        
-        <Animate size={[1,1,1]} color={"orange"}/>
-        <OrbitControls />
-      </Canvas>
-    </div>
-  )
-}
+    <Center>
+    <primitive
+      object={model.scene}
+      scale={0.6}
+      position={[0, 1, 0]}
+    />
+    </Center>
+  );
+};
 
-export default Mineral
+export default Mineral;
